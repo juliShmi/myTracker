9b7249061650dcec3eb945c969fdd2e402c1e0a6
juliShmi <juliashmitko@gmail.com>
2023-01-12 16:24:34 +0200

    addNewBook metod in want-to-read

--- src/app/my-books/want-to-read-books/want-to-read-books.component.ts
+++ src/app/my-books/want-to-read-books/want-to-read-books.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { Book } from "../book.model";
 
 @Component({
@@ -7,8 +7,18 @@ import { Book } from "../book.model";
   styleUrls: ["./want-to-read-books.component.css"],
 })
 export class WantToReadBooksComponent {
+  @ViewChild("newAuthor") newAuthorInputRef: ElementRef;
+  @ViewChild("newTitle") newTitleInputRef: ElementRef;
+
   books: Book[] = [
     new Book("Харуки Мураками", "О чем я говорю, когда говорю о беге"),
     new Book("Эдгар По", "Золотой жук"),
   ];
+
+  onAddNewBook() {
+    const authorAdded = this.newAuthorInputRef.nativeElement.value;
+    const titleAdded = this.newTitleInputRef.nativeElement.value;
+    const newBook = new Book(authorAdded, titleAdded);
+    this.books.push(newBook);
+  }
 }
